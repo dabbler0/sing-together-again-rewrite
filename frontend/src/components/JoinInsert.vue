6586d24d004f1234ce44aebb0de9593dae6a57f3
Anthony Bau <dab1998@gmail.com>
2020-10-11 00:37:53 +0000

    Calibration improvements and join error message

--- frontend/src/components/JoinInsert.vue
+++ frontend/src/components/JoinInsert.vue
@@ -6,12 +6,21 @@
 
     <v-card-text>
       <v-form>
+        <div v-if="roomErrorMessage">
+          Please enter a room number!
+        </div>
+
         <v-text-field
           label="Room ID"
           name="Room ID"
           v-model="roomId"
           type="text">
         </v-text-field>
+
+        <div v-if="nameErrorMessage">
+          Please enter your name below!
+        </div>
+
         <v-text-field
           label="Your Name"
           name="Your Name"
@@ -33,7 +42,11 @@
 
     <v-card-actions class="justify-center">
       <v-btn v-on:click="$emit('cancel')" large>Back</v-btn>
+<<<<<<< Updated upstream
       <v-btn color="primary" v-on:click="$emit('submit', roomId, name, { hearSelf, leader })" large>Join</v-btn>
+=======
+      <v-btn color="primary" v-on:click="updateSingSettingsAndEmit()" large>Join</v-btn>
+>>>>>>> Stashed changes
     </v-card-actions>
   </v-card>
 </template>
@@ -45,8 +58,27 @@ export default {
     return {
       roomId: (this.$route.params.hasOwnProperty('prefill') ? this.$route.params.prefill : ''),
       name: '',
-      hearSelf: false,
-      leader: false
+      hearme: false,
+      leader: false,
+      nameErrorMessage: false,
+      roomErrorMessage: false
+    }
+  },
+  methods: {
+    updateSingSettingsAndEmit () {
+      if (this.roomId.length === 0) {
+        this.roomErrorMessage = true
+        return
+      }
+
+      if (this.name.length === 0) {
+        this.nameErrorMessage = true
+        return
+      }
+
+      this.$store.commit('setHearme', this.hearme)
+      this.$store.commit('setLeader', this.leader)
+      this.$emit('submit', this.roomId, this.name)
     }
   }
 }
